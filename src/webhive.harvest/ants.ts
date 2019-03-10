c0bc2138f5cef429d3802b41c5041984824bf3d0
roman.vasilev <root@localhost>
2019-03-10 18:27:19 +0400

    feat: Added echojs

--- src/webhive.harvest/ants.ts
+++ src/webhive.harvest/ants.ts
@@ -6,7 +6,8 @@ export type Ant = {
 
 export const ants: Ant[] = [
     { name: 'javascriptkicks', target: 'https://javascriptkicks.com/feeds/rss', defaultCategory: 'JavaScript' },
-    // { name: 'codeburst', target: 'https://codeburst.io/feed', defaultCategory: 'Code' },
-    // { name: 'rutracker1565', target: 'http://feed.rutracker.org/atom/f/1565.atom', defaultCategory: 'Video' },
-    // { name: '2ality', target: 'http://feeds.feedburner.com/2ality', defaultCategory: 'JavaScript' },
+    { name: 'codeburst', target: 'https://codeburst.io/feed', defaultCategory: 'Code' },
+    { name: 'rutracker1565', target: 'http://feed.rutracker.org/atom/f/1565.atom', defaultCategory: 'Video' },
+    { name: '2ality', target: 'http://feeds.feedburner.com/2ality', defaultCategory: 'JavaScript' },
+    { name: 'echojs', target: 'http://www.echojs.com/rss', defaultCategory: 'JavaScript' },
 ];
