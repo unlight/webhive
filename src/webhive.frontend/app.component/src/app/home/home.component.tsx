a6fada333d27256d5b4abc90c41f11d99d274b5b
Roman <>
2019-07-07 00:49:26 +0400

    Loading but not enough

--- src/webhive.frontend/app.component/src/app/home/home.component.tsx
+++ src/webhive.frontend/app.component/src/app/home/home.component.tsx
@@ -1,8 +1,11 @@
 import * as loadScript from '@shinin/load-script';
 import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 
 export function Home(props) {
     // todo: fix me, convert to loadable component
-    loadScript('entry-list.js');
-    return <entry-list-component></entry-list-component>;
+    useEffect(() => {
+        loadScript('entry-list.js');
+    }, []);
+    return <entry-list-component>Loading...</entry-list-component>;
 };
