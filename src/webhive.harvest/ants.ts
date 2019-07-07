0e9c630f950a1df2561480880b6de82b9b5014f0
Roman <>
2019-07-07 13:19:49 +0400

    Added webapplog

--- src/webhive.harvest/ants.ts
+++ src/webhive.harvest/ants.ts
@@ -64,6 +64,7 @@ export const ants: Ant[] = [
     { name: 'xakep', target: 'https://xakep.ru/category/coding/feed/', defaultCategory: 'Dev' },
     { name: 'stackshare', target: 'https://stackshare.io/featured-posts.atom', defaultCategory: 'News' },
     { name: 'zerotoprod', target: 'https://zerotoprod.com/index.xml', defaultCategory: 'DevOps' },
+    { name: 'webapplog', target: 'https://webapplog.com/feed/', defaultCategory: 'Web' },
     // { name: 'toddmotto', target: 'https://toddmotto.com/feed.xml', defaultCategory: 'Web' },
     // { name: 'not_rss', target: 'http://padabum.com/', defaultCategory: 'Web' },
     // { name: 'not_rss', target: 'http://nodetuts.com/', defaultCategory: 'Web' },
