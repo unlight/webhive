5e87943f5bd0dbee0b3d3c1b390c7a74a2bee343
Roman <>
2019-07-18 23:12:41 +0400

    Make config plain

--- src/webhive.frontend/app.component/src/app.component.config.js
+++ src/webhive.frontend/app.component/src/app.component.config.js
@@ -1,7 +1,21 @@
-export const components = {
-    'header.component': { enabled: true, main: 'header.js' },
-    'nav.component': { enabled: true, main: 'nav.js' },
-    'entry-list.component': { enabled: true, main: 'entry-list.js' },
-    'search.page': { enabled: true, main: 'search-page.js' },
-    'example.component': { enabled: true, main: 'example.component.plugin.js' },
-}
+exports.components = {}
+
+exports.components["header.component"] = {}
+exports.components["header.component"].enabled = true
+exports.components["header.component"].main = "header.js"
+
+exports.components["nav.component"] = {}
+exports.components["nav.component"].enabled = true
+exports.components["nav.component"].main = "nav.js"
+
+exports.components["entry-list.component"] = {}
+exports.components["entry-list.component"].enabled = true
+exports.components["entry-list.component"].main = "entry-list.js"
+
+exports.components["search.page"] = {}
+exports.components["search.page"].enabled = true
+exports.components["search.page"].main = "search-page.js"
+
+exports.components["example.component"] = {}
+exports.components["example.component"].enabled = true
+exports.components["example.component"].main = "example.component.plugin.js"
