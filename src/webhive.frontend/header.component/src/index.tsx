3bc2dcc791143313f7975c842186109d5ef6209e
Roman <>
2019-08-08 00:42:31 +0400

    module loader

--- src/webhive.frontend/header.component/src/index.tsx
+++ src/webhive.frontend/header.component/src/index.tsx
@@ -1,7 +1,10 @@
-import { createElement as h } from 'h-document-element';
 import './header.component';
+import { createLoadRemoteModule, createRequires } from 'remote-module-loader';
 
-window.addEventListener('route.transition.end', (event: any) => {
+const remoteModuleLoader = createLoadRemoteModule();
+
+window.addEventListener('route.transition.end', async (event: any) => {
     const { app } = event.detail;
+    const {h} = await remoteModuleLoader('//unpkg.com/h-document-element');
     app.querySelector('#header').append(<header-component />);
 });
