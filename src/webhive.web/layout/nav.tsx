bd925688854cfb566c0a9d199db21b2e3a93d45c
Roman <>
2019-09-01 23:19:38 +0400

    Added microfrontend link to main page

--- src/webhive.web/layout/nav.tsx
+++ src/webhive.web/layout/nav.tsx
@@ -4,7 +4,8 @@ export function Nav() {
     return <nav>
         <ul>
             <li><a href="/">Home</a></li> {}
-            <li><a href="/search">Search</a></li>
+            <li><a href="/search">Search</a></li> {}
+            <li><a href="/frontend/">MicroFrontend</a></li> {}
         </ul>
     </nav>;
 }
