c3ea73730c08d7147f45c3cc21635d02e43f4bd8
roman <>
2020-01-20 22:51:44 +0400

    Added google search (medium articles ad)

--- src/webhive.web/entry/entry.component.tsx
+++ src/webhive.web/entry/entry.component.tsx
@@ -3,6 +3,10 @@ import * as h from 'vhtml';
 
 export function EntryComponent({ entry }: { entry: Entry }) {
     return <div class="entry">
-        <span class="entry__category">{entry.category.name}</span> <a href={entry.link}>{entry.title}</a>
+        <span class="entry__category">{entry.category.name}</span>
+        <span>
+            <a href={entry.link}>{entry.title}</a> { }
+            <a class="entry__google" href={`http://google.com/search?q=${encodeURIComponent(entry.title)}`}>[G]</a>
+        </span>
     </div>;
 }
