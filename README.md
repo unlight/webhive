249c58ef237101fab1667784601e9784259ce12c
Roman_Vasilev <>
2022-05-20 00:57:00 +0400

    test: fix

--- README.md
+++ README.md
@@ -4,15 +4,13 @@
 docker run -it -p 27017:27017 --rm mongo
 npm run dev:api
 set API_URL=http://localhost:3000
-set API_TOKEN=
 npm run harvest -- --ant=ant1
 ```
 
 ## TODO
 
--   add https://event-driven.io/rss.xml
--   Create indexes
--   BEM names for css classes
--   createEntry: use ctx.throw to throw errrors (errorlings?)
--   pagination
--   graphql
+- Create indexes
+- BEM names for css classes
+- createEntry: use ctx.throw to throw errrors (errorlings?)
+- pagination
+- graphql
--- src/webhive.api/entry/entry.module.spec.ts
+++ src/webhive.api/entry/entry.module.spec.ts
@@ -5,131 +5,143 @@ import { CreateEntryDTO } from './entry.dto';
 import { injector } from 'njct';
 import { MongoClient, Db } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { mockMongoDatabaseClose, mockMongoDatabase } from '../testing/mock-mongo-database';
+import {
+  mockMongoDatabaseClose,
+  mockMongoDatabase,
+} from '../testing/mock-mongo-database';
 import { config } from '../config';
+import { ObjectId } from '../store/mongo';
+
 const sham = require('koa-sham');
 
 describe('entry api', () => {
-    let app: ThenArg<ReturnType<typeof main>>;
-    let mongoServer: MongoMemoryServer;
-    let client: MongoClient;
-    let database: Db;
-    let testCreateEntryDTO = <CreateEntryDTO>{
-        title: 'Nobs',
-        link: 'http://totty.com/antichlor/pluteiform?a=mimicism&b=eccentrometer#viremia',
-        date: '2019-03-01T15:09:24+03:00',
-        category: 'Unemended',
-    };
-    let shamOptions = (custom?, headers?) => ({
-        headers: {
-            'api-token': config.get('apiToken'),
-            ...(headers || {}),
+  let app: ThenArg<ReturnType<typeof main>>;
+  let mongoServer: MongoMemoryServer;
+  let client: MongoClient;
+  let database: Db;
+  let testCreateEntryDTO = <CreateEntryDTO>{
+    title: 'Nobs',
+    link: 'http://totty.com/antichlor/pluteiform?a=mimicism&b=eccentrometer#viremia',
+    date: '2019-03-01T15:09:24+03:00',
+    category: 'Unemended',
+  };
+  let shamOptions = (custom?, headers?) => ({
+    headers: {
+      'api-token': config.get('apiToken'),
+      ...(headers || {}),
+    },
+    promise: true,
+    resolveWithFullResponse: true,
+    ...(custom || {}),
+  });
+
+  before(async () => {
+    ({ mongoServer, client, database } = await mockMongoDatabase());
+    injector.provide('client', () => client);
+    injector.provide('database', () => database);
+    // Insert test data
+    await database.collection('entry2').insertMany(
+      [
+        {
+          _id: new ObjectId('000000000000000000000001'),
+          title: 'title01',
+          link: 'https://foo/01',
+          date: '2001-02-03T07:20:01-05:00',
+          category_id: new ObjectId('000000000000000000000003'),
         },
-        promise: true,
-        resolveWithFullResponse: true,
-        ...(custom || {}),
-    });
+        {
+          _id: new ObjectId('000000000000000000000002'),
+          title: 'title02',
+          link: 'https://foo/02',
+          date: '2002-02-03T07:20:01-05:00',
+          category_id: new ObjectId('000000000000000000000004'),
+        },
+        {
+          _id: new ObjectId('000000000000000000000003'),
+          title: 'title03',
+          link: 'https://foo/03',
+          date: '2003-02-03T07:20:01-05:00',
+          category_id: new ObjectId('000000000000000000000003'),
+        },
+      ],
+      { forceServerObjectId: true },
+    );
+    await database.collection('category').insertMany(
+      [
+        { _id: new ObjectId('000000000000000000000003'), name: 'cat03' },
+        { _id: new ObjectId('000000000000000000000004'), name: 'cat04' },
+      ],
+      { forceServerObjectId: true },
+    );
+  });
 
-    before(async () => {
-        ({ mongoServer, client, database } = await mockMongoDatabase());
-        injector.provide('client', () => client);
-        injector.provide('database', () => database);
-        // Insert test data
+  after(async () => {
+    await mockMongoDatabaseClose({ client, mongoServer });
+  });
 
-        await database.collection('entry2').insertMany(
-            [
-                {
-                    title: 'title01',
-                    link: 'https://foo/01',
-                    date: '2001-02-03T07:20:01-05:00',
-                    category_id: '03',
-                },
-                {
-                    title: 'title02',
-                    link: 'https://foo/02',
-                    date: '2002-02-03T07:20:01-05:00',
-                    category_id: '04',
-                },
-                {
-                    title: 'title03',
-                    link: 'https://foo/03',
-                    date: '2003-02-03T07:20:01-05:00',
-                    category_id: '03',
-                },
-            ],
-            { forceServerObjectId: true },
-        );
-        await database
-            .collection('category')
-            .insertMany([{ name: 'cat03' }, { name: 'cat04' }], { forceServerObjectId: true });
-    });
+  before(async () => {
+    app = await main();
+  });
 
-    after(async () => {
-        await mockMongoDatabaseClose({ client, mongoServer });
-    });
+  it('create entry must be protected endpoint', async () => {
+    const response: CustomServerResponse = await sham(
+      app,
+      '/entry',
+      shamOptions({ method: 'POST', body: testCreateEntryDTO }, { 'api-token': 'foo' }),
+    );
+    expect(response.statusCode).toEqual(401);
+  });
 
-    before(async () => {
-        app = await main();
-    });
+  it('GET /entry search query unwind', async () => {
+    const response: CustomServerResponse = await sham(
+      app,
+      '/entry',
+      shamOptions({ method: 'GET', qs: { q: 'title02' } }),
+    );
 
-    it('create entry must be protected endpoint', async () => {
-        const response: CustomServerResponse = await sham(
-            app,
-            '/entry',
-            shamOptions({ method: 'POST', body: testCreateEntryDTO }, { 'api-token': 'foo' }),
-        );
-        expect(response.statusCode).toEqual(401);
-    });
+    expect(response.statusCode).toEqual(200);
+    expect(response.ctx.body).toBeAn(Array);
+    expect(response.ctx.body.length).toBe(1);
+    const [entry] = response.ctx.body;
+    expect(entry.category).toBeTruthy();
+    expect(entry.category.name).toBe('cat04');
+  });
 
-    it('GET /entry search query unwind', async () => {
-        const response: CustomServerResponse = await sham(
-            app,
-            '/entry',
-            shamOptions({ method: 'GET', qs: { q: 'title02' } }),
-        );
-        expect(response.statusCode).toEqual(200);
-        expect(response.ctx.body).toBeAn(Array);
-        expect(response.ctx.body.length).toBe(1);
-        const [entry] = response.ctx.body;
-        expect(entry.category).toBeTruthy();
-        expect(entry.category.name).toBe('cat04');
-    });
+  it('POST /entry fail', async () => {
+    const response: CustomServerResponse = await sham(
+      app,
+      '/entry',
+      shamOptions({ method: 'POST', body: {} }),
+    );
+    expect(response.statusCode).toEqual(400);
+  });
 
-    it('POST /entry fail', async () => {
-        const response: CustomServerResponse = await sham(
-            app,
-            '/entry',
-            shamOptions({ method: 'POST', body: {} }),
-        );
-        expect(response.statusCode).toEqual(400);
-    });
+  it('POST /entry ok', async () => {
+    const response: CustomServerResponse = await sham(
+      app,
+      '/entry',
+      shamOptions({
+        method: 'POST',
+        body: <CreateEntryDTO>{
+          title: 'nonliving',
+          link: 'https://ambary.com/porphyrogenite/chlorophyllan?a=civilizedness&b=mesologic#partiality',
+          date: '2024-11-28T22:31:32-09:00',
+          category: 'Monkeyfy',
+        },
+      }),
+    );
+    expect(response.statusCode).toEqual(201);
+    expect(response.ctx.body).toContain({ message: 'Entry created' });
+  });
 
-    it('POST /entry ok', async () => {
-        const response: CustomServerResponse = await sham(
-            app,
-            '/entry',
-            shamOptions({
-                method: 'POST',
-                body: <CreateEntryDTO>{
-                    title: 'nonliving',
-                    link: 'https://ambary.com/porphyrogenite/chlorophyllan?a=civilizedness&b=mesologic#partiality',
-                    date: '2024-11-28T22:31:32-09:00',
-                    category: 'Monkeyfy',
-                },
-            }),
-        );
-        expect(response.statusCode).toEqual(201);
-        expect(response.ctx.body).toContain({ message: 'Entry created' });
-    });
-
-    it('GET /entry', async () => {
-        const response: CustomServerResponse = await sham(
-            app,
-            '/entry',
-            shamOptions({ method: 'GET' }),
-        );
-        expect(response.statusCode).toEqual(200);
-        expect(response.ctx.body).toBeTruthy();
-        expect(response.ctx.body).toBeAn(Array);
-    });
+  it('GET /entry', async () => {
+    const response: CustomServerResponse = await sham(
+      app,
+      '/entry',
+      shamOptions({ method: 'GET' }),
+    );
+    expect(response.statusCode).toEqual(200);
+    expect(response.ctx.body).toBeTruthy();
+    expect(response.ctx.body).toBeAn(Array);
+  });
 });
